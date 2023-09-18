1e70433e6a96ef27794cf212f94ebcb0280f7d19
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-09-18 19:23:32 +0600

    work with arrow function

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const locationName = document.getElementById('locationName');
 const locationTemperature = document.getElementById('locationTemp');
 const locationWeather = document.getElementById('locationWeather');
 
-buttonSearch.addEventListener('click', function () {
+buttonSearch.addEventListener('click', () => {
     const inputSearch = document.getElementById('inputSearch');
     const inputValue = inputSearch.value.toString().toLowerCase();
     
@@ -16,17 +16,17 @@ buttonSearch.addEventListener('click', function () {
     fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data))
-
-    function displayTemperature(info) {
-        const cityName = info.city.name;
-        const temperature = info.list[0].main.temp;
-        const weather = info.list[0].weather[0].main;
-        const icon = info.list[0].weather[0].icon;
-
-        locationName.innerText = cityName;
-        locationTemperature.innerText = temperature;
-        locationWeather.innerText = weather;
-        const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-        weatherIcon.setAttribute('src', iconUrl);
-    }
 })
+
+const displayTemperature = (info) =>  {
+    const cityName = info.city.name;
+    const temperature = info.list[0].main.temp;
+    const weather = info.list[0].weather[0].main;
+    const icon = info.list[0].weather[0].icon;
+
+    locationName.innerText = cityName;
+    locationTemperature.innerText = temperature;
+    locationWeather.innerText = weather;
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
+    weatherIcon.setAttribute('src', iconUrl);
+}
